f821dc6d5432b94c024ac66da51c5335e62d5ff5
rickyflows <usetheflowsricky@gmail.com>
2023-05-31 23:00:04 -0700

    p83, note on p82

--- p83.py
+++ p83.py
@@ -24,22 +24,13 @@ def main():
             up = (i - 1, j)
             right = (i, j + 1)
             down = (i + 1, j)
-            for v in filter(lambda u: in_square(u, m), [up, right, down]):
+            left = (i, j - 1)
+            for v in filter(lambda u: in_square(u, m), [up, right, down, left]):
                 graph[unfold((i, j), m), unfold(v, m)] = data[v]
 
-    # O(m^5)
-    candidates = set()
-    for i in range(m):
-        source = unfold((i, 0), m)
-        dist, _ = dijkstra(graph, source)
-        candidate = data[fold(source, m)] + min(dist.reshape(m, m)[:, -1])
-        candidates.add(candidate)
-    print(min(candidates))
+    source = unfold((0, 0), m)
+    dist, _ = dijkstra(graph, source)
+    print(dist[-1] + data[0, 0])
 
 if __name__=="__main__":
-    # Some improvements:
-    # - https://projecteuler.net/action=quote;post_id=391868 (O(m^3))
-    # - https://projecteuler.net/action=quote;post_id=400861 (O(m^3))
-    # - https://projecteuler.net/action=quote;post_id=399206 (O(m^2))
-    # - https://github.com/nayuki/Project-Euler-solutions/blob/master/python/p082.py (O(m^2))
     main()
