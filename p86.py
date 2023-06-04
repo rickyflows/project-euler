5eee09c35d9ff660ed4aa4d3fc43fb7051864e43
rickyflows <usetheflowsricky@gmail.com>
2023-06-03 17:46:40 -0700

    p86

---
+++ p86.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+from utils.general_utils import generate_pythagorean_triplets
+from numba import njit
+
+@njit
+def helper(cuboid):
+    a, b, c, = cuboid
+    d1 = (b + c)**2 + a**2
+    d2 = (a + c)**2 + b**2
+    d3 = (a + b)**2 + c**2
+    d = min([d1, d2, d3])
+    return int(d**0.5)**2 == d
+
+def main():
+    # Just fiddled with M till I found the answer
+    M = 1818
+    triplets = generate_pythagorean_triplets(6 * M)
+    candidate_cuboids = set()
+    for a, b, _ in triplets:
+        if b <= M:
+            candidates = [(d, a - d, b) for d in range(max(1, a - M), min(M, a // 2 + 1))]
+            candidate_cuboids.update(map(lambda x: tuple(sorted(x)), candidates))
+        if a <= M:
+            candidates = [(d, b - d, a) for d in range(max(1, b - M), min(M, b // 2 + 1))]
+            candidate_cuboids.update(map(lambda x: tuple(sorted(x)), candidates))
+
+    print(sum(1 for cuboid in filter(helper, candidate_cuboids)))
+
+if __name__=='__main__':
+    main()
